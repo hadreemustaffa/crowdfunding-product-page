62f0cb2ddb7d97419e3a75d9450353ff411bb319
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-04-19 13:10:44 +0800

    add success message after submit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // DOM elements
 const header = document.querySelector('header');
-const navBtn = document.querySelector('nav .btn-nav');
+const navButton = document.querySelector('nav .btn-nav');
 const form = document.querySelector('form');
 const list = document.querySelector('#listContainer');
 const currentAmountText = document.querySelector('#currentAmount').textContent;
@@ -8,17 +8,27 @@ const currentBarProgress = document.querySelector('#currentProgress');
 const backProject = document.querySelector('#backProject');
 
 const modal = document.querySelector('.modal');
-const closeModalBtn = document.querySelector('#closeModalBtn');
+const closeModalButton = document.querySelector('#closeModalBtn');
 const selectRewardButtons = document.querySelectorAll('.reward');
 const inputGroup = document.querySelectorAll("input[name='reward-group']");
+const amountInStock = document.querySelectorAll('.card__stock span');
+const successMessage = document.querySelector('.success');
+const messageButton = document.querySelector('#closeMessage');
 
 // assets
-const navBtnOpen = './images/icon-hamburger.svg';
-const navBtnClose = './images/icon-close-menu.svg';
+const navButtonOpen = './images/icon-hamburger.svg';
+const navButtonClose = './images/icon-close-menu.svg';
 
 // features
 const MAX_AMOUNT = 100000;
 let currentAmount = currentAmountText.replace(/\D/, '');
+
+amountInStock.forEach((amount) => {
+  const convertedAmount = Number(amount.textContent);
+  if (convertedAmount < 1) {
+    amount.closest('.card').classList.add('card--disabled');
+  }
+});
 
 const modalOpen = () => {
   modal.style.display = 'flex';
@@ -28,11 +38,15 @@ const modalOpen = () => {
     document.body.style.overflow = 'hidden';
   }
 };
+
 const modalClose = () => {
   modal.removeAttribute('style');
   document.body.removeAttribute('style');
   modal.removeAttribute('opened');
+  successMessage.removeAttribute('style');
+  form.removeAttribute('style');
 };
+
 const addClassSelected = (element) => {
   const card = element.parentElement.parentElement.parentElement;
 
@@ -40,10 +54,14 @@ const addClassSelected = (element) => {
   card.classList.add('selected');
 };
 
+const scrollIntoView = (input) => {
+  const card = input.closest('.card');
+  card.scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
+
 // event listeners
 backProject.addEventListener('click', () => {
   document.querySelector('.selected')?.classList.remove('selected');
-
   modalOpen();
 
   inputGroup.forEach((reward) => {
@@ -58,7 +76,7 @@ for (let i = 0; i < selectRewardButtons.length; i++) {
   selectRewardButtons[i].addEventListener('click', () => {
     modalOpen();
     inputGroup[i + 1].checked = true;
-    inputGroup[i + 1].scrollIntoView({ behavior: 'smooth', block: 'center' });
+    scrollIntoView(inputGroup[i + 1]);
     addClassSelected(inputGroup[i + 1]);
   });
 }
@@ -66,33 +84,45 @@ for (let i = 0; i < selectRewardButtons.length; i++) {
 inputGroup.forEach((input) => {
   input.addEventListener('click', () => {
     addClassSelected(input);
-    input.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center',
-    });
+    scrollIntoView(input);
   });
 });
 
-closeModalBtn.addEventListener('click', () => {
+closeModalButton.addEventListener('click', () => {
   modalClose();
 });
 
-navBtn.addEventListener('click', () => {
+form.addEventListener('submit', () => {
+  successMessage.style.display = 'flex';
+  form.style.display = 'none';
+});
+
+navButton.addEventListener('click', () => {
   list.classList.toggle('sr-only');
 
   if (list.classList.contains('sr-only')) {
     list.ariaExpanded = 'false';
-    navBtn.firstElementChild.src = navBtnOpen;
+    navButton.firstElementChild.src = navButtonOpen;
   } else {
     list.ariaExpanded = 'true';
-    navBtn.firstElementChild.src = navBtnClose;
+    navButton.firstElementChild.src = navButtonClose;
   }
+});
+
+messageButton.addEventListener('click', () => {
+  modalClose();
 });
 
 document.addEventListener('click', (e) => {
   const isModalOpen = modal.hasAttribute('opened');
 
   if (isModalOpen) {
+    if (e.target == modal) {
+      modalClose();
+    }
+  }
+
+  if (isModalOpen && successMessage.hasAttribute('style')) {
     if (e.target == modal) {
       modalClose();
     }
