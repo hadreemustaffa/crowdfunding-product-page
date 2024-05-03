3efa348e27c8714f38f412f5cf60f58779ae9ab3
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-05-03 21:14:35 +0800

    feat: add stat update functions, add button listeners

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // DOM elements
 const header = document.querySelector('header');
 const navButton = document.querySelector('nav .btn-nav');
+const cards = document.querySelectorAll('.about .card');
 const form = document.querySelector('form');
 const list = document.querySelector('#listContainer');
 const currentPledgeAmount = document.querySelector('#currentAmount');
-const currentBackersAmount = document.querySelector('.backers__title');
+const currentBackersAmount = document.querySelector(
+  '.backers__people .backers__title'
+);
 const currentBarProgress = document.querySelector('#currentProgress');
 const backProject = document.querySelector('#backProject');
-
+const bookmarkProject = document.querySelector('#bookmarkProject');
+const bookmarkProjectText = document.querySelector('#bookmarkProject span');
 const modal = document.querySelector('.modal');
 const closeModalButton = document.querySelector('#closeModalBtn');
 const selectRewardButtons = document.querySelectorAll('.reward');
@@ -23,14 +27,17 @@ const navButtonClose = './images/icon-close-menu.svg';
 // features
 const MAX_AMOUNT = 100000;
 let updatedPledgeAmount;
-let updatedBackers;
+let updatedBackersAmount;
+let updatedStock;
 
-amountInStock.forEach((amount) => {
-  const convertedAmount = Number(amount.textContent);
-  if (convertedAmount < 1) {
-    amount.closest('.card').classList.add('card--disabled');
-  }
-});
+const addDisabled = (formElement, element) => {
+  const cardBtn = element.querySelector('button.reward');
+  element.classList.add('card--disabled');
+  formElement.classList.add('card--disabled');
+  cardBtn.setAttribute('disabled', '');
+  cardBtn.classList.add('btn--disabled');
+  cardBtn.textContent = 'Out of stock';
+};
 
 const calculateProgress = () => {
   let totalPledgeAmount = Number(
@@ -60,26 +67,43 @@ const modalClose = () => {
   form.removeAttribute('style');
 };
 
-const removeDuplicateClass = () => {
-  document.querySelector('.selected')?.classList.remove('selected');
+const removeDuplicateClass = (className) => {
+  document.querySelector(`.${className}`)?.classList.remove(`${className}`);
 };
 
-const addClassSelected = (element) => {
+const addClass = (className, element) => {
   removeDuplicateClass();
-  element.classList.add('selected');
+  element.classList.add(`${className}`);
 };
 
 const scrollIntoView = (element) => {
   element.scrollIntoView({ behavior: 'smooth', block: 'center' });
 };
 
+const formatStringToNumber = (string) => {
+  return Number(string.replace(/\D/, ''));
+};
+
+const handleStock = (element, formElement, stock) => {
+  element.querySelector('.card__stock span').textContent = stock.toString();
+
+  if (stock == 0) {
+    addDisabled(formElement, element);
+  }
+};
+
 const validateForm = () => {
-  const card = document.querySelector('.card.selected');
-  const cardInputNumber = card.querySelector("input[type='number']");
-  let totalPledgeAmount = Number(
-    currentPledgeAmount.textContent.replace(/\D/, '')
-  );
+  const formCard = document.querySelector('.card.selected');
+  const cardInputNumber = formCard.querySelector("input[type='number']");
+  const cardStock = formCard.querySelector('.card__stock span');
+
+  let totalPledgeAmount = formatStringToNumber(currentPledgeAmount.textContent);
   let inputValue = Number(cardInputNumber.value);
+  let currentStock;
+
+  if (cardStock) {
+    currentStock = Number(cardStock.textContent);
+  }
 
   const calculatePledgeAmount = () => {
     updatedAmount = inputValue + totalPledgeAmount;
@@ -90,11 +114,39 @@ const validateForm = () => {
     calculateProgress(updatedAmount);
   };
 
+  const updateStats = () => {
+    updatedBackersAmount =
+      formatStringToNumber(currentBackersAmount.textContent) + 1;
+
+    currentBackersAmount.textContent = updatedBackersAmount.toString();
+
+    if (cardStock) {
+      if (updatedStock == 0) {
+        cardStock.textContent = 0;
+      }
+
+      updatedStock = currentStock - 1;
+
+      cardStock.textContent = updatedStock.toString();
+
+      if (cardInputNumber.id == 'reward1') {
+        handleStock(cards[0], formCard, updatedStock);
+      }
+      if (cardInputNumber.id == 'reward2') {
+        handleStock(cards[1], formCard, updatedStock);
+      }
+      if (cardInputNumber.id == 'reward3') {
+        handleStock(cards[2], formCard, updatedStock);
+      }
+    }
+  };
+
   if (
     !cardInputNumber.parentElement.classList.contains('error') &&
     inputValue != ''
   ) {
     calculatePledgeAmount();
+    updateStats();
     successMessage.style.display = 'flex';
     form.style.display = 'none';
   }
@@ -114,10 +166,19 @@ backProject.addEventListener('click', () => {
     if (radio.id == 'input1') {
       radio.focus();
       radio.checked = true;
-      addClassSelected(radio.closest('.card'));
       scrollIntoView(radio.closest('.card'));
+      addClass('selected', radio.closest('.card'));
     }
   });
+});
+
+bookmarkProject.addEventListener('click', () => {
+  bookmarkProject.classList.toggle('active');
+  if (bookmarkProject.classList.contains('active')) {
+    bookmarkProjectText.textContent = 'Bookmarked';
+  } else {
+    bookmarkProjectText.textContent = 'Bookmark';
+  }
 });
 
 for (let i = 0; i < selectRewardButtons.length; i++) {
@@ -125,19 +186,21 @@ for (let i = 0; i < selectRewardButtons.length; i++) {
     modalOpen();
     inputRadios[i + 1].checked = true;
     scrollIntoView(inputRadios[i + 1].closest('.card'));
-    addClassSelected(inputRadios[i + 1].closest('.card'));
+    addClass('selected', inputRadios[i + 1].closest('.card'));
   });
 }
 
-inputRadios.forEach((radio) => {
-  radio.addEventListener('click', () => {
-    addClassSelected(radio.closest('.card'));
-    scrollIntoView(radio.closest('.card'));
-  });
-});
-
 closeModalButton.addEventListener('click', () => {
   modalClose();
+});
+
+form.addEventListener('click', (e) => {
+  inputRadios.forEach((radio) => {
+    if (e.target == radio) {
+      scrollIntoView(radio.closest('.card'));
+      addClass('selected', radio.closest('.card'));
+    }
+  });
 });
 
 form.addEventListener('input', (e) => {
@@ -164,14 +227,14 @@ form.addEventListener('submit', (e) => {
 });
 
 navButton.addEventListener('click', () => {
-  list.classList.toggle('sr-only');
+  list.classList.toggle('expanded');
 
-  if (list.classList.contains('sr-only')) {
-    list.ariaExpanded = 'false';
-    navButton.firstElementChild.src = navButtonOpen;
-  } else {
+  if (list.classList.contains('expanded')) {
     list.ariaExpanded = 'true';
     navButton.firstElementChild.src = navButtonClose;
+  } else {
+    list.ariaExpanded = 'false';
+    navButton.firstElementChild.src = navButtonOpen;
   }
 });
 
